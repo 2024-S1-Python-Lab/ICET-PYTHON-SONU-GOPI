40ca481fdae8604974fa02acf18dde74227faaed
toro1111 <sonugopi54@gmail.com>
2024-12-13 15:44:51 +0530

    second commit

---
+++ PROGRAM31.py
@@ -0,0 +1,9 @@
+import calc
+num1=float(input("enter the number"))
+num2=float(input("enter the number"))
+sum=calc.add(num1,num2)
+print(f"sum=",sum)
+print(f"Subtraction={calc.add(num1,num2)}")
+print(f"Multiplication={calc.add(num1,num2)}")
+print(f"Division={calc.add(num1,num2)}")
+
